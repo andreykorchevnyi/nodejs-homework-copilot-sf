7ef92cb6ff313a30366af74f05b7d3a756cbc6bc
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2023-05-18 19:32:52 +0300

    555

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
+const logger = require('morgan');
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-// Маршрут для обработки запросов от Salesforce Canvas приложения
-app.post('/canvas', async (req, res) => {
-    console.log(req);
+app.use(logger(formatsLogger));
+
+app.get('/sf-canvas', handleSfOauthToken);
+
+async function handleSfOauthToken(req, res) {
     try {
-        // Получение oauthToken и refreshToken из тела запроса
+        // Получение oauthToken из тела запроса
         const { oauthToken } = req.body;
 
         // Запрос к Salesforce REST API для получения информации о пользователе
@@ -20,31 +25,31 @@ app.post('/canvas', async (req, res) => {
             }
         );
 
-        // Извлечение нужной информации о пользователе
-        const { userId, organizationId, name, picture, email } =
+        // Извлечение информации о пользователе
+        const { user_id, organization_id, name, picture, email } =
             userInfoResponse.data;
 
         // Верификация полученных данных (в данном случае просто проверка наличия id пользователя)
-        if (!userId) {
+        if (!user_id) {
             throw new Error('Invalid user data');
         }
 
         // Отправка верифицированных данных обратно клиенту
         res.json({
-            id: userId,
-            organizationId,
+            id: user_id,
+            organizationId: organization_id,
             name,
             avatar: picture,
             email,
         });
     } catch (error) {
-        // Обработка ошибок
         console.error('Error:', error.message);
         res.status(500).json({ error: 'Something went wrong' });
     }
-});
+}
 
 // Запуск сервера
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log('Hello');
 });
